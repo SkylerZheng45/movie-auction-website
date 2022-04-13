c64c926c4ec36d99f36776e2d181a02148582160
SkylerZheng45 <skylerzheng45@gmail.com>
2022-04-13 17:07:57 -0500

    removed venv

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+import sys
 from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 
@@ -19,6 +21,27 @@ def login():
    user = request.form['nm']
    password = request.form['password']
    return redirect(url_for('success',name = user))
+
+#signup
+@app.route('/signup')
+def signup_page():
+   return render_template('signup.html')
+
+@app.route('/signup')
+def signup_again(msg):
+   return render_template('signup.html', msg=msg)
+
+@app.route('/signup',methods = ['POST', 'GET'])
+def signup():
+   user = request.form['nm']
+   password = request.form['password']
+   password2 = request.form['password2']
+   app.logger.info(password)
+   app.logger.info(password2)
+   if password == password2:
+      return redirect(url_for('success',name = user))
+   else:
+      return redirect(url_for('signup_again', msg='Passwords do not match.'))
 
 
 if __name__ == '__main__':
