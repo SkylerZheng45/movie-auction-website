f66302fd36864d0fc9a06dd145b512979c86e4d1
Joey Chou <chouj@Joeys-MacBook-Pro.local>
2022-04-22 13:49:38 -0500

    Database Changes

---
+++ DatabaseDataInserts.sql
@@ -0,0 +1,30 @@
+-- OVERALL FORMAT 
+-- INSERT INTO table (column1,column2 ,..)
+-- VALUES( value1,	value2 ,...);
+
+-- All the movie Inserts based on the front page.
+INSERT INTO MOVIEINFO (NAME,YEAR,DESC) VALUES 
+("Spiderman No Way Home",2021,"With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.");
+
+INSERT INTO MOVIEINFO (NAME,YEAR,DESC) VALUES 
+("Minons",2015,"Minions Stuart, Kevin, and Bob are recruited by Scarlet Overkill, a supervillain who, alongside her inventor husband Herb, hatches a plot to take over the world.");
+
+INSERT INTO MOVIEINFO (NAME,YEAR,DESC) VALUES 
+("Morbius",2022,"Biochemist Michael Morbius tries to cure himself of a rare blood disease, but he inadvertently infects himself with a form of vampirism instead.");
+
+INSERT INTO MOVIEINFO (NAME,YEAR,DESC) VALUES 
+("Sonic The Hedgehog",2020,"After discovering a small, blue, fast hedgehog, a small-town police officer must help him defeat an evil genius who wants to do experiments on him.");
+
+INSERT INTO MOVIEINFO (NAME,YEAR,DESC) VALUES 
+("Ghostbusters: Afterlife",2021,"When a single mom and her two kids arrive in a small town, they begin to discover their connection to the original Ghostbusters and the secret legacy their grandfather left behind.");
+
+INSERT INTO MOVIEINFO (NAME,YEAR,DESC) VALUES 
+("Artemis Fowl",2020,"Artemis Fowl, a young criminal prodigy, hunts down a secret society of fairies to find his missing father.");
+
+INSERT INTO MOVIEINFO (NAME,YEAR,DESC) VALUES 
+("Shang-Chi and the Legend of the Ten Rings",2022,"Shang-Chi, the master of weaponry-based Kung Fu, is forced to confront his past after being drawn into the Ten Rings organization.");
+
+INSERT INTO MOVIEINFO (NAME,YEAR,DESC) VALUES 
+("My Neighbor Totoro",1988,"When two girls move to the country to be near their ailing mother, they have adventures with the wondrous forest spirits who live nearby.");
+
+
